2d998394ea7b6fcdd3707e1cca5ed0ec47a54b9f
Rama <rama.p@sekon.com>
2020-02-14 13:56:43 -0500

    2.1: course contents, step 6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Header from './components/Header'
-import Content from './components/Content'
-import Total from './components/Total'
+import Course from './components/Course';
 
 const App = () => {
   const course = {
@@ -9,24 +7,30 @@ const App = () => {
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
 
   return (
     <div>
-      <Header title={course.name} />
-      <Content parts={course.parts} />
-      <Total total={course.parts} />
+      <Course course={course} />
     </div>
   )
 }
