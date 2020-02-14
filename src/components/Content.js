cc31553607633aee7635bc81c51490c1b2d0b2f2
rama-workathome <51408462+rama-workathome@users.noreply.github.com>
2020-02-14 09:04:11 -0500

    1.3: course information, step 3

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import Part from './Part'
 
-const Content = (props) => {
-
+const Content = ({part1, part2, part3}) => {
     return (
         <>
-           <Part name={props.part1} exercises={props.exercise1} />
-           <Part name={props.part2} exercises={props.exercise2} />
-           <Part name={props.part3} exercises={props.exercise3} />
+           <Part name={part1.name} exercises={part1.exercises} />
+           <Part name={part2.name} exercises={part2.exercises} />
+           <Part name={part3.name} exercises={part3.exercises} />
         </>
     )
 }
