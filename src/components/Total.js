529b1641cdb22cdd3ef75d6875c78affd37657b7
Rama <rama.p@sekon.com>
2020-02-14 13:59:37 -0500

    2.2: course contents, step 7 and 2.3*: course contents, step 8

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const Total = ({total}) => <p>Number of exercises {total.reduce((a,b)=>a+b.exercises,0)}</p>
+const Total = ({total}) => <p><b>total of {total.reduce((a,b)=>a+b.exercises,0)} exercises</b></p>
 
 export default Total
