55ea014129a7802fe42f9920cbba451b8aa8c778
Rama <rama.p@sekon.com>
2020-02-14 12:35:20 -0500

    1.13*: anecdotes, step 2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import Button from './components/Button';
 
 const App = ({anecdotes}) => {
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
   const nextAnecdote = () => setSelected(Math.floor(Math.random()*anecdotes.length))
+
+  const addVotes = (value) => {
+    const localVotes = [...votes]
+    localVotes[value]++
+    setVotes(localVotes)
+  }
 
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p> has {votes[selected]} votes</p>
+      <Button handler={()=>addVotes(selected)} label="vote" />
       <Button handler={()=>nextAnecdote()} label="next anecdote" />
     </div>
   )
