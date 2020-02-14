18e178555a50a73ecaa153571fe493a5d635be25
rama-workathome <51408462+rama-workathome@users.noreply.github.com>
2020-02-14 09:43:52 -0500

    1:4: course information, step 4

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const Total = (props) =><p>Number of exercises {props.total}</p>
+const Total = ({total}) => <p>Number of exercises {total.reduce((a,b)=>a+b.exercises,0)}</p>
 
 export default Total
