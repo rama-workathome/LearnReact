63bfc51206f15dddb21f96450aa74bd8bd0d003e
rama-workathome <51408462+rama-workathome@users.noreply.github.com>
2020-02-14 09:50:16 -0500

    1.5: course information, step5

--- src/App.js
+++ src/App.js
@@ -4,28 +4,29 @@ import Content from './components/Content'
 import Total from './components/Total'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = 
-  [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-    name: 'Using props to pass data',
-    exercises: 7
-    },
-    {
-    name: 'State of a component',
-    exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header title={course} />
-      <Content parts={parts} />
-      <Total total={parts} />
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.parts} />
     </div>
   )
 }
