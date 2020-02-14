7bd1e7dbc67baa14f391ff1ccefc85e1d7046619
Rama <rama.p@sekon.com>
2020-02-14 11:27:04 -0500

    1.7: unicafe, step 2 1.8: unicafe, step 3

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
 const Statistics = ({stats}) => {
+    const all = stats.good + stats.neutral + stats.bad
+    let average=0, positive=0
+    if (all > 0) {
+        average = (stats.good - stats.bad)/all
+        positive = (stats.good * 100 / all)
+    }  
     return (
         <>
             <p>good {stats.good}</p>
             <p>neutral {stats.neutral}</p>
             <p>bad {stats.bad}</p>
+            <p>all {all}</p>
+            <p>average {average}</p>
+            <p>positive {positive}%</p>
         </>
     )
 }
