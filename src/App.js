752f1aac44829bf2972b4123c73aa9a4911015ad
Rama <rama.p@sekon.com>
2020-02-19 10:21:53 -0500

    2.12* Data for countries, step 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios  from 'axios'
+import Filter from './components/Filter';
+import Countries from './components/Countries'
 
 const App = () => {
+  const [data, setData] = useState([])
+  
+  const [filter, setFilter] = useState('')
+
+  const updateFilter = (e) => {
+    setFilter(e)
+  }
+
+  const hookEffect = () => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response=> {
+        setData(response.data)
+        
+      })
+
+  }
+
+  useEffect(hookEffect, [])
+  
   return (
     <div>
-      some code here
+      <Filter v={filter} c={updateFilter} />
+      <Countries data={data} filter={filter} />
     </div>
   )
 }
---
+++ src/components/Countries.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Countries = ({data, filter}) =>{
+    const f = filter.toLowerCase()
+    const t = data.filter(country =>country.name.toLowerCase().indexOf(f) !== -1)
+    if (t.length > 10) {
+        return (
+            <p>Too many coutries to show</p>
+        )
+    } else if (t.length > 1 && t.length <= 10) {
+        return (
+            t.map(p=><p key={p.numericCode}>{p.name}</p>)
+        )
+    } else if (t.length === 1) {
+        const single = t[0]
+        console.log(t)
+        return (
+            <div>
+                <h2>{single.name}</h2>
+                <p>capital: {single.capital}</p>
+                <p>population: {single.population}</p>
+                <h3>languages</h3>
+                <ul>
+                {single.languages.map(s=> <li key={single.name}>{s.name}</li>)}
+                </ul>
+                <p><img src={single.flag} alt={single.name + ' flag'} width="200" border="1" /></p>
+            </div>
+        )
+    } else {
+        return (
+            <p>Nothing to show. Start typing to filter.</p>
+        )
+    }   
+}
+
+export default Countries
